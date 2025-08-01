425be681a24f8df8b259fd7e72a44b1b01b691b7
Augustinas <augustinas.sueris@gmail.com>
2025-08-01 08:28:36 +0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,22 @@
+const waitForElementToExist = (selector) => {
+    return new Promise(resolve => {
+        if (selector()) resolve(selector())
+
+        const observer = new MutationObserver(() => {
+            if (selector()) {
+                resolve(selector())
+                observer.disconnect()
+            }
+        });
+
+        observer.observe(document.body, {
+            subtree: true,
+            childList: true,
+        })
+    })
+}
+
+const wait = (f) => {
+    const delay = 400 + Math.floor(Math.random() * 600)
+    return new Promise(resolve => setTimeout(() => resolve(f()), delay))
+}
